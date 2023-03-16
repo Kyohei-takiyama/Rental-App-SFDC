4143e13901e645704913d47412bc1a0038aaa79e
Kyohei-takiyama <takiyamakyohei@gmail.com>
2023-03-17 07:36:54 +0900

    feature/user-pre-registration

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,55 @@
+import { useState } from "react";
+import axios from "axios";
+import { API } from "../config";
+
 const Register = () => {
-  return <div className="display-1 bg-primary text-light p-5">Register</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      // request to server
+      const res = await axios.post(`/auth/pre-register`, {
+        email,
+        password,
+      });
+      console.log(res);
+    } catch {}
+  };
+
+  return (
+    <div>
+      <h1 className="display-1 bg-primary text-light p-5">Register</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 offset-lg-4">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                className="form-control mb-4"
+                placeholder="メールアドレスを入力してください"
+                required
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                className="form-control mb-4"
+                placeholder="パスワードを入力してください"
+                required
+                autoFocus
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className="btn btn-primary col-12">登録</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
